79dba24ff0fb59edf6c01caab1e90033eff310ac
machado001 <machadowg@gmail.com>
2022-11-29 18:47:17 -0300

    navbar

--- react-marketplace/src/App.jsx
+++ react-marketplace/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Header from './components/Header'
+import Header from "./components/Header";
+import Destaques from "./components/Header/Destaques";
 
 export default function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      
     </>
-  )
+  );
 }
+//gab esse exemplo q tu pegou é de html eu axo dogs ss
