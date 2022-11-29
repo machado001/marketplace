a84d52c0d14c6b4d90d690d3504449cf3e9f6777
machado001 <machadowg@gmail.com>
2022-11-29 18:41:41 -0300

    primeiras mudanças

--- react-marketplace/src/components/Header/index.jsx
+++ react-marketplace/src/components/Header/index.jsx
@@ -1,10 +1,10 @@
-const itemNavBarClassName = "transition ease-in-out p-3 hover:bg-red-200 rounded-md"
+const itemNavBarClassName = "transition ease-in-out my-6 md:my-0 p-3 hover:bg-red-200 rounded-md"
 
 export default function Header() {
   return (
     <header>
       <nav className="px-6 py-2 w-full bg-red-100 flex justify-between items-center">
-        <div className="flex">
+        <div className="md:flex">
           <img
             src="https://via.placeholder.com/75"
             alt="placeholder img"
@@ -15,7 +15,7 @@ export default function Header() {
             <li><a className= {itemNavBarClassName} href="#">Produtos recentes</a></li>
             <li><a className= {itemNavBarClassName} href="#">Adicionar produtos</a></li>
             <li><a className= {itemNavBarClassName} href="#">Categorias</a></li>
-          </ul>
+          </ul> 
         </div>
         <img
           src="https://via.placeholder.com/50"
