86145d263e75c972da93fdcde631365762acaa4f
Gustavo Santana <95528216+gustavsant@users.noreply.github.com>
2022-11-30 15:29:58 -0300

    Update index.jsx

--- react-marketplace/src/components/Header/index.jsx
+++ react-marketplace/src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 import { List } from 'phosphor-react'
 import { useState } from 'react'
 const itemNavBarClassName =
-  'transition ease-in-out my-2 p-3 hover:bg-red-200 rounded-md cursor-pointer'
+  'transition ease-in-out my-2 p-3 hover:bg-indigo-500 rounded-md cursor-pointer'
 
 export default function Header() {
   const [mobileMenu, setMobileMenu] = useState(false)
@@ -11,7 +11,7 @@ export default function Header() {
   }
   return (
     <header>
-      <nav className="shadow px-6 py-2 w-full bg-red-100 flex justify-between items-center z-[2]">
+      <nav className="shadow px-6 py-2 w-full bg-indigo-400 flex justify-between items-center z-[2]">
         <div className="md:flex">
           <img
             src="https://via.placeholder.com/75"
@@ -21,8 +21,8 @@ export default function Header() {
           <ul
             className={
               mobileMenu
-                ? 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 bg-red-100 md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500 top-[90px] opacity-100'
-                : 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 bg-red-100 md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500'
+                ? 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 bg-indigo-400 md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500 top-[90px] opacity-100'
+                : 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 bg-indigo-400 md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500'
             }
           >
             <li className={itemNavBarClassName}>
