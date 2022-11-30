e5c0997990f31aec16aa19c2d93e39cbaccbd2f4
Gustavo Santana <95528216+gustavsant@users.noreply.github.com>
2022-11-30 19:31:42 -0300

    header updates plus google auth with popup

--- react-marketplace/src/components/Login/index.jsx
+++ react-marketplace/src/components/Login/index.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { CaretRight, GoogleLogo } from 'phosphor-react'
+
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import { auth } from '../../services/fireBaseConfig'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
 export function Login() {
   const [email, setEmail] = useState('')
@@ -9,9 +12,24 @@ export function Login() {
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth)
 
-  function handleLogin(e) {
-    e.preventDefault()
+  function handleGoogleSignup() {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  function handleLogin() {
     signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   if (user) {
@@ -19,44 +37,72 @@ export function Login() {
   }
 
   return (
-    <div className="border md:w-3/5 w-4/5 mx-auto my-6 h-[70vh] flex flex-col items-center justify-center">
-      <header className="">
-        <h1 className="">Faça Login</h1>
-      </header>
-
-      <form>
-        <div className="">
-          <label htmlFor="email">E-mail</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="johndoe@gmail.com"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div className="">
-          <label htmlFor="password">Senha</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="********************"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <a href="#">Esqueceu sua senha ?</a>
-
-        <button className="" onClick={handleLogin}>
-          Entrar
-        </button>
-        <div className="">
-          <p>Você não tem uma conta?</p>
-          <Link to="/register">Crie a sua conta aqui</Link>
-        </div>
-      </form>
+    <div className="md:w-3/5 w-4/5 mx-auto my-6 h-[70vh] flex flex-col items-center justify-center">
+      <div className=" shadow w-4/5 py-4 md:py-8 md:px-16 px-4">
+        <header>
+          <h1 className="text-3xl w-full mb-6 text-indigo-900">Faça Login</h1>
+        </header>
+        <form className="">
+          <div className="mb-3">
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Seu email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="outline-0 border-2 rounded px-3 py-2 w-full"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Sua senha"
+              onChange={(e) => setPassword(e.target.value)}
+              className="outline-0 border-2 rounded px-3 py-2 w-full"
+            />
+          </div>
+          <p className="mb-3">
+            <a className="text-indigo-600" href="#">
+              Esqueceu sua senha?
+            </a>
+          </p>
+          <button
+            className="px-4 py-2 border bg-indigo-400 rounded mb-3"
+            onClick={handleLogin}
+            type="button"
+          >
+            <div className="flex items-center">
+              <p className="pr-2 text-white font-bold">Entrar</p>
+              <CaretRight className="text-white" />
+            </div>
+          </button>
+          <div className="md:flex md:items-center mb-6">
+            <p>Não possui uma conta? </p>
+            <Link
+              className="md:ml-1 text-indigo-600 underline font-semibold"
+              to="/register"
+            >
+              Registre-se
+            </Link>
+          </div>
+          <div className="border relative mb-4">
+            <div className="absolute inset-x-1/2 translate-y-[-50%] translate-x-[-50%] font-semibold text-gray-500 bg-white text-center w-[130px]">
+              <p>Ou use o Google</p>
+            </div>
+          </div>
+          <div className="w-full flex justify-center">
+            <button
+              type="button"
+              className="border rounded-full p-2 bg-indigo-400 hover:scale-110 transition-transform"
+              onClick={handleGoogleSignup}
+            >
+              <GoogleLogo size={24} weight="bold" className="text-white" />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
