a5dda4de5f49ff5d6bfe536ced4ec7b9370189b4
Gustavo Santana <95528216+gustavsant@users.noreply.github.com>
2022-12-01 23:51:23 -0300

    olha ai se deu

--- react-marketplace/src/routes/root.jsx
+++ react-marketplace/src/routes/root.jsx
@@ -1,7 +1,5 @@
-import Header from "../components/Header"
+import Header from '../components/Header'
 
-export default function Root () {
-    return (
-     <Header />
-    )
+export default function Root() {
+  return <Header />
 }
