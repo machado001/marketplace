2f234e4dd3735e9a79ad16989040d6f4d2e81872
Gustavo Santana <95528216+gustavsant@users.noreply.github.com>
2022-12-01 22:52:29 -0300

    free ad vlw

--- react-marketplace/src/error-page.jsx
+++ react-marketplace/src/error-page.jsx
@@ -1,22 +1,30 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from 'react-router-dom'
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.log(`errozada dog`,error)
-    return(
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Essa pagina não existe! </p>
-            <p>
-                Mensagem do Erro: 
-                <i> {error.statusText || error.message}</i>
-                <br />
-                Número do erro:
-                <i> 
-                    {error.status}
-                </i>
-
-            </p>
-        </div>
-    )
+  const error = useRouteError()
+  console.log(`errozada dog`, error)
+  return (
+    <div
+      id="error-page"
+      className="flex flex-col justify-center items-center h-[100vh]"
+    >
+      <h1>Oops!</h1>
+      <p>Essa pagina não existe! </p>
+      Mensagem do Erro:
+      <i> {error.statusText || error.message}</i>
+      <br />
+      Número do erro:
+      <i className="font-black text-9xl">{error.status}</i>
+      <p className="font-bold text-2xl">
+        razões para me contratar, acesse: <br />
+        <a
+          href="https://www.linkedin.com/in/gustavsant/"
+          className="text-indigo-600 hover:animate-pulse"
+          target="_blank"
+        >
+          Meu Linkedin!
+        </a>
+      </p>
+    </div>
+  )
 }
