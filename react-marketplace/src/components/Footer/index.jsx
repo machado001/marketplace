f2bed6039d91a492360bd86c845b810de6624065
Gustavo Santana <95528216+gustavsant@users.noreply.github.com>
2022-12-03 18:33:04 -0300

    dd d dd

--- react-marketplace/src/components/Footer/index.jsx
+++ react-marketplace/src/components/Footer/index.jsx
@@ -1,15 +1,17 @@
 export default function Footer() {
-
-
-return (
-    <div id='footer' className="bg-slate-300 w-full absolute flex justify-between bottom-0 h-20 p-8 ">
-     <p>GGH - Gabriel, Gustavo & Heitor.</p>
-     <ul className="flex">
-        <li>k</li>
-        <li>m</li>
-        <li>h</li>
-     </ul>
+  return (
+    <div className="sticky bottom 0">
+      <div
+        id="footer"
+        className="bg-slate-300 w-full absolute flex justify-between bottom-0 h-20 p-8 "
+      >
+        <p>GGH - Gabriel, Gustavo & Heitor.</p>
+        <ul className="flex">
+          <li>k</li>
+          <li>m</li>
+          <li>h</li>
+        </ul>
+      </div>
     </div>
-)
-
+  )
 }
