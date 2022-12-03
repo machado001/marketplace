5bd59816e2f37519497ce0c9a91135c2e56caeeb
machado001 <machadowg@gmail.com>
2022-12-03 04:04:12 -0300

    Melhorando o Carousel Na minha opiniao ja esta utilizavel para uma primeira versao; o click and drag funciona pra desktop e celular

--- react-marketplace/src/components/Carousel/index.jsx
+++ react-marketplace/src/components/Carousel/index.jsx
@@ -2,7 +2,7 @@
 import { useEffect,useRef, useState } from 'react';
 import { getDefaultAppConfig, isBrowserExtension, uuidv4 } from '@firebase/util'
 import {motion} from 'framer-motion'
-import imagesPool from '../../imagesPool';
+import productsMock from '../../productsMock';
 import { v4 } from 'uuid'
 
 
@@ -18,13 +18,19 @@ export default function Carousel() {
     },[width])
     return (
         
-        <section ref={carousel} whiletap={{cursor:"grabbing"}} id="carousel-section" className="border-solid w-full max-w-4xl min-h-[20vh] overflow-hidden mx-auto border-2 " >
+        <section ref={carousel} whiletap={{cursor:"grabbing"}} id="carousel-section" className="border-solid w-full max-w-2xl min-h-[10vh] overflow-hidden mx-auto bg-slate-200 shadow	 " >
             Clique e arraste
             {/* <img src="" alt="left-arrow" /> */}
-<motion.div drag="x" initial={{x:300}} animate={{x:0}} transition={{duration:1}} dragConstraints={{right:0,left:-width}} id="carousel-itself" className='cursor-grab flex mx-auto w-100 '>
-{imagesPool.map((imgSrc)=> (
-                <motion.div key={v4()} className='item-div min-w-[200px] min-h-[200px] '  >
-                 <img src={imgSrc.src} drag="x"  alt="Make sure to include a alt tag, because react might throw an error at build" className='rounded-2xl w-48 h-full pointer-events-none	 '/>
+<motion.div drag="x" initial={{x:300}} animate={{x:0}} transition={{duration:1}} dragConstraints={{right:0,left:-width}} id="carousel-itself" className='cursor-grab flex mx-auto'>
+{productsMock.map((product)=> (
+                <motion.div key={v4()} className='item-div min-w-[200px] min-h-[100px] max-w-[100px] max-h-[300px] m-1 rounded shadow-lg border-solid border-black bg-zinc-50'  >
+                 <img src={product.productSrc} drag="x"  alt="Highlight-Items" className='rounded w-[80%] h-[40%] pointer-events-none mx-auto shadow-sm my-3 '/>
+                 <ul className='space-y-4 ml-5 mt-8'>
+                     <li ><span className='italic bg-slate-300 p-2 rounded'>{product.productName}</span></li>
+                     <li ><span className='text-lime-500 bg-slate-300 p-2 rounded'>{product.productPrice}</span></li>
+                     <li ><a target="_blank" href='https://www.youtube.com/watch?v=dKD4o-jrvTE' className='rounded bg-blue-200 p-2 font-medium bg-slate-300 	
+'>More Details...</a></li>
+                 </ul>
                 </motion.div>
             ))}
 </motion.div>   
---
+++ react-marketplace/src/productsMock.js
@@ -0,0 +1,24 @@
+const productsMock = [
+
+  {productName: 'Boneco Bolsonaro',
+  productPrice: 24.69,
+    productSrc: "src/assets/images/boneco-bolsonaro.jpg"}
+    
+    ,
+  {
+    productName: 'Geladeira Brastemp',
+  productPrice: 101.69,
+  productSrc: "src/assets/images/geladeira.jpg"},
+  {productName: 'Neymar Fortnite',
+  productPrice: 6666.66,
+  productSrc: "src/assets/images/neymar-fortnite.jpg"},
+  {
+    productName: 'Playstation 5',
+  productPrice: 7.99,
+  productSrc: "src/assets/images/play-5.jpg"},
+  {productName: 'Smart TV 50 poleguadas',
+  productPrice: 60.40,
+  productSrc: "src/assets/images/smart-tv-50.jpg"},
+];
+
+export default productsMock;
