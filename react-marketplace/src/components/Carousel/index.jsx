3798cc2861c1358952cb0495a0381cfb23cebbce
Gustavo Santana <95528216+gustavsant@users.noreply.github.com>
2022-12-03 14:56:17 -0300

    mudanças no estilo header/carousel

--- react-marketplace/src/components/Carousel/index.jsx
+++ react-marketplace/src/components/Carousel/index.jsx
@@ -1,40 +1,71 @@
-
-import { useEffect,useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
 import { getDefaultAppConfig, isBrowserExtension, uuidv4 } from '@firebase/util'
-import {motion} from 'framer-motion'
-import productsMock from '../../productsMock';
+import { motion } from 'framer-motion'
+import productsMock from '../../productsMock'
 import { v4 } from 'uuid'
-
-
-
+import { CaretRight } from 'phosphor-react'
 
 export default function Carousel() {
+  const carousel = useRef()
+  const [width, setWidth] = useState(``)
 
-    const carousel = useRef();
-    const [width,setWidth] = useState(``)
-
-    useEffect(()=> {
-        setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
-    },[width])
-    return (
-        
-        <section ref={carousel} whiletap={{cursor:"grabbing"}} id="carousel-section" className="border-solid w-full max-w-2xl min-h-[10vh] overflow-hidden mx-auto bg-slate-200 shadow	 " >
-            Clique e arraste
-            {/* <img src="" alt="left-arrow" /> */}
-<motion.div drag="x" initial={{x:300}} animate={{x:0}} transition={{duration:1}} dragConstraints={{right:0,left:-width}} id="carousel-itself" className='cursor-grab flex mx-auto'>
-{productsMock.map((product)=> (
-                <motion.div key={v4()} className='item-div min-w-[200px] min-h-[100px] max-w-[100px] max-h-[300px] m-1 rounded shadow-lg border-solid border-black bg-zinc-50'  >
-                 <img src={product.productSrc} drag="x"  alt="Highlight-Items" className='rounded w-[80%] h-[40%] pointer-events-none mx-auto shadow-sm my-3 '/>
-                 <ul className='space-y-4 ml-5 mt-8'>
-                     <li ><span className='italic bg-slate-300 p-2 rounded'>{product.productName}</span></li>
-                     <li ><span className='text-lime-500 bg-slate-300 p-2 rounded'>{product.productPrice}</span></li>
-                     <li ><a target="_blank" rel="noopener noreferrer" href='https://www.youtube.com/watch?v=dKD4o-jrvTE' className='rounded bg-blue-200 p-2 font-medium bg-slate-300 	
-'>More Details...</a></li>
-                 </ul>
-                </motion.div>
-            ))}
-</motion.div>   
-            {/* <img src="" alt="right-arrow" /> */}
-       </section>
-    )
+  useEffect(() => {
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+  }, [width])
+  return (
+    <section
+      ref={carousel}
+      whiletap={{ cursor: 'grabbing' }}
+      id="carousel-section"
+      className="border-solid w-full max-w-2xl min-h-[10vh] overflow-hidden mx-auto bg-slate-200 shadow	 "
+    >
+      {/* <img src="" alt="left-arrow" /> */}
+      <motion.div
+        drag="x"
+        initial={{ x: 300 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1 }}
+        dragConstraints={{ right: 0, left: -width }}
+        id="carousel-itself"
+        className="cursor-grab shadow-lg border border-gray-100 flex items-center mx-auto h-[380px] px-4"
+      >
+        {productsMock.map((product) => (
+          <motion.div
+            key={v4()}
+            className="item-div border min-w-[200px] h-[350px] m-1 rounded shadow-lg border-solid border-gray-300 bg-zinc-50"
+          >
+            <img
+              src={product.productSrc}
+              drag="x"
+              alt="Highlight-Items"
+              className="rounded w-[80%] h-[50%] pointer-events-none mx-auto shadow-sm my-3 "
+            />
+            <ul className="space-y-4 ml-5">
+              <li className="h-8 w-36 truncate">
+                <span className=" rounded cursor-default w-full">
+                  {product.productName}
+                </span>
+              </li>
+              <li>
+                <span className="text-lime-700 font-bold rounded cursor-default">
+                  R${product.productPrice}
+                </span>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.youtube.com/watch?v=dKD4o-jrvTE"
+                  className="rounded bg-indigo-200 p-2 font-semibold flex items-center justify-between w-28"
+                >
+                  Detalhes <CaretRight />
+                </a>
+              </li>
+            </ul>
+          </motion.div>
+        ))}
+      </motion.div>
+      {/* <img src="" alt="right-arrow" /> */}
+    </section>
+  )
 }
--- react-marketplace/src/components/Header/index.jsx
+++ react-marketplace/src/components/Header/index.jsx
@@ -1,9 +1,9 @@
 import { getAuth } from 'firebase/auth'
 import { List } from 'phosphor-react'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/authContext'
-import GGH from '../../assets/GGH.svg'
+import GGH from '../../assets/GGHNEW.svg'
 import cart from '../../assets/cart.svg'
 import notification from '../../assets/notification.svg'
 import message from '../../assets/message.svg'
@@ -11,73 +11,84 @@ import message from '../../assets/message.svg'
 export default function Header() {
   const [mobileMenu, setMobileMenu] = useState(false)
   const itemNavBarClassName =
-    'transition ease-in-out my-2 p-3 hover:bg-indigo-500 rounded-md cursor-pointer text-black text-sm'
+    'transition ease-in-out my-2 p-3 hover:bg-indigo-200 rounded-md cursor-pointer text-black text-sm'
 
   const { user, logout, loading } = useAuth()
+  const navigate = useNavigate()
 
   function changeProfile() {
     if (user) {
-      return  <div className="flex gap-4 items-center">
-        <img src={cart} alt="placeholder img" class="object-contain h-6 w-6" />
-        <img src={notification} alt="placeholder img" class="object-contain h-6 w-5" />
-        <img src={message} alt="placeholder img" class="object-contain h-6 w-6" />
-      <div className="w-16 h-16 relative flex flex-col justify-center items-center p-2 group cursor-pointer">
-      
-        <img
-          className="rounded-full h-12 shadow "
-          src={
-            user && user.photoURL
-              ? user.photoURL
-              : 'https://www.shareicon.net/data/256x256/2016/05/27/771338_man_512x512.png'
-          }
-          alt=""
-        />
-        
-        <div className="absolute shadow-lg w-[156px] top-16 h-0 truncate rounded-md group-hover:h-20 group-hover:flex bg-white border-2 px-2 py-1 transition-all flex-col">
-          <p className="text-gray-800 p-1 w-full transition hover:bg-indigo-600 rounded-md">
-            <Link to={'/profile'}>Profile</Link>
-          </p>
-          <p
-            className="text-gray-800 p-1 w-full transition hover:bg-indigo-600 rounded-md"
-            onClick={handleLogout}
-          >
-            Desconectar
-          </p>
+      return (
+        <div className="flex gap-4 items-center">
+          <img
+            src={cart}
+            alt="placeholder img"
+            className="object-contain h-6 w-6"
+          />
+          {/* <img
+            src={notification}
+            alt="placeholder img"
+            className="object-contain h-6 w-5"
+          />
+          <img
+            src={message}
+            alt="placeholder img"
+            className="object-contain h-6 w-6"
+          /> */}
+          <div className="w-16 h-16 relative flex flex-col justify-center items-center p-2 group cursor-pointer">
+            <img
+              className="rounded-full h-9 shadow "
+              src={
+                user && user.photoURL
+                  ? user.photoURL
+                  : 'https://www.shareicon.net/data/256x256/2016/05/27/771338_man_512x512.png'
+              }
+              alt=""
+            />
+
+            <div className="z-2 absolute shadow-lg w-[135px] top-16 h-0 truncate rounded-md group-hover:h-20 group-hover:flex hover:h-20 hover:flex bg-white border-2 px-2 py-1 transition-all flex-col">
+              <p className="text-gray-800 p-1 w-full transition hover:bg-indigo-200 rounded-md">
+                <Link to={'/profile'}>Profile</Link>
+              </p>
+              <p
+                className="text-gray-800 p-1 w-full transition hover:bg-indigo-200 rounded-md"
+                onClick={handleLogout}
+              >
+                Desconectar
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <List
+              size={32}
+              className="cursor-pointer md:hidden"
+              onClick={handleMenuClick}
+              data-name="menu"
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex items-center">
-        <List
-          size={32}
-          className="cursor-pointer md:hidden"
-          onClick={handleMenuClick}
-          data-name="menu"
-        />
-      </div>
-    </div>
-    }
-    else {
-      return <div className="flex gap-4 items-center">
-      <div className="w-22 h-16 relative flex flex-row justify-center items-start p-8 group cursor-pointer">
-          <p className="text-black p-2 w-full ">
-            <Link to={'/login'}>Entrar</Link>
-          </p>
-          <p
-            className="text-black p-1 w-full"
-          >
-            <Link to="/register">Não tem uma conta? se registre.</Link>
-            
-          </p>
-       
-      </div>
-      <div className="flex items-center">
-        <List
-          size={32}
-          className="cursor-pointer md:hidden"
-          onClick={handleMenuClick}
-          data-name="menu"
-        />
-      </div>
-    </div>
+      )
+    } else {
+      return (
+        <div className="flex gap-4 items-center">
+          <div className="h-16 relative gap-4 flex flex-row justify-between items-center">
+            <p className="text-black border-2 rounded-md md:flex hidden px-3 py-2 font-bold cursor-pointer bg-indigo-600 text-indigo-100">
+              <Link to="/register">Registrar-se</Link>
+            </p>
+            <p className="text-black border-2 rounded-md md:flex hidden px-3 py-2 font-bold cursor-pointer">
+              <Link to={'/login'}>Entrar</Link>
+            </p>
+          </div>
+          <div className="flex items-center">
+            <List
+              size={32}
+              className="cursor-pointer md:hidden"
+              onClick={handleMenuClick}
+              data-name="menu"
+            />
+          </div>
+        </div>
+      )
     }
   }
 
@@ -94,30 +105,24 @@ export default function Header() {
 
   return (
     <header>
-      <div className="px-6 py-0 w-full h-24 bg-white flex justify-evenly items-center z-[2] border-solid">
-      <img src={GGH} alt="placeholder img" class="object-contain h-15 w-40" />
-<form class="w-1/4">   
-    <label for="default-search" class="mb-10 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-3xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search products." required />
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
-</form>
-{changeProfile()}
+      <div className="px-6 py-0 w-full h-24 bg-white flex justify-between items-center z-[2] border-solid">
+        <img
+          src={GGH}
+          alt="placeholder img"
+          className="object-contain w-24 md:w-40"
+          onClick={() => navigate('/')}
+        />
+
+        {changeProfile()}
       </div>
-      <nav className="shadow px-6 py-0 w-full h-full bg-white flex justify-center items-center z-[2] border-solid border-grey border">
-        
-        <div className="flex justify-center w-full h-16 z-[99999]">
+      <nav className="shadow h-0 md:h-full px-6 py-0 w-full bg-white flex justify-center items-center z-[2] border-solid border-grey border">
+        <div className="flex justify-center w-full h-16">
           <div className="flex items-end">
-            
             <ul
               className={
                 mobileMenu
-                  ? 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4  md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500 top-[90px] opacity-100'
-                  : 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4  md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500'
+                  ? 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4  md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500 bg-white top-[90px] opacity-100'
+                  : 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4  md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500 bg-white'
               }
             >
               <li className={itemNavBarClassName}>
@@ -129,9 +134,28 @@ export default function Header() {
               <li className={itemNavBarClassName}>
                 <Link to="/add-product">Adicionar produtos</Link>
               </li>
+              {!user && (
+                <>
+                  <li>
+                    <p className="text-black border-2 rounded-md md:hidden flex px-3 py-2 font-bold cursor-pointer bg-indigo-600 text-indigo-100">
+                      <Link to="/register">Registrar-se</Link>
+                    </p>
+                  </li>
+                  <li>
+                    <p className="text-black border-2 rounded-md md:hidden flex px-3 py-2 font-bold cursor-pointer">
+                      <Link to={'/login'}>Entrar</Link>
+                    </p>
+                  </li>
+                </>
+              )}
+              {/* <p className="text-black border-2 rounded-md md:flex hidden px-3 py-2 font-bold cursor-pointer bg-indigo-600 text-indigo-100">
+              <Link to="/register">Registrar-se</Link>
+            </p>
+            <p className="text-black border-2 rounded-md md:flex hidden px-3 py-2 font-bold cursor-pointer">
+              <Link to={'/login'}>Entrar</Link>
+            </p> */}
             </ul>
           </div>
-         
         </div>
       </nav>
     </header>
