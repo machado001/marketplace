37090b89b74d6a0a529d6ff7959ac921e79eed92
Gustavo Santana <95528216+gustavsant@users.noreply.github.com>
2022-12-08 03:17:15 -0300

    pagina do produto mobile, quase pronto

--- react-marketplace/src/components/Footer/index.jsx
+++ react-marketplace/src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="sticky bottom 0">
       <div
         id="footer"
-        className="bg-slate-300 w-full absolute flex justify-center bottom-0 h-20 p-8 "
+        className="bg-slate-300 w-full absolute flex justify-center h-20 p-8 "
       >
         <p>
           &copy; GGH - Gabriel, Gustavo & Heitor. {new Date().getFullYear()}
--- react-marketplace/src/components/ProductPage/index.jsx
+++ react-marketplace/src/components/ProductPage/index.jsx
@@ -13,10 +13,10 @@ export default function ProductPage() {
   const product = newProduct.filter((product) => product.id === id)[0]
   console.log(newProduct)
   let [productsCounter, setProductsCounter] = useState(1)
-  let [productPrice, setProductPrice] = useState(product.productPrice)
+  let [totalPrice, setTotalPrice] = useState(product.productPrice)
 
   useEffect(() => {
-    setProductPrice(product.productPrice * productsCounter)
+    setTotalPrice(product.productPrice * productsCounter)
   }, [productsCounter])
 
   function incrementProduct() {
@@ -44,16 +44,18 @@ export default function ProductPage() {
   return (
     <>
       <Header />
-      <div className="flex mt-8  justify-center items-center">
-        <div className="product h-screen gap-4 md:w-2/4 w-5/6 px-7 flex justify-between">
-          <div>
-            <img className="" src={product.productImage} alt="" />
+      <div className="flex mt-8 justify-center items-center">
+        <div className="product h-[130vh] gap-4 w-5/6 px-7 flex md:flex-row flex-col md:justify-between">
+          <div className="mt-8 mx-auto">
+            <img src={product.productImage} alt="" />
           </div>
-          <div className="PRODUCT-INFOS flex flex-col">
-            <span className="text text-3xl">{product.productName}</span>
+          <div className="PRODUCT-INFOS flex flex-col order-1">
+            <span className="text-center md:text-start absolute top-[110px] md:static md:top-0 text-2xl md:text-3xl">
+              {product.productName}
+            </span>
             <span className="text mb-4">{product.productDesc}</span>
             <span className="text text-green-800 text-3xl font-bold">
-              R${productPrice}
+              R${product.productPrice}
             </span>
             <div className="flex flex-col gap-1">
               <div className="flex justify items-center gap-2">
@@ -84,16 +86,19 @@ export default function ProductPage() {
                 </button>
               </div>
               <hr />
-              <div className="flex justify">
-                <button className="w-32 mt-4 flex items-center justify-between gap-2 bg-indigo-400 py-2 px-4 rounded font-semibold">
+              <div className="flex flex-col">
+                <span>Total: R${totalPrice}</span>
+                <button className="md:w-32 flex items-center justify-center md:justify-between gap-2 bg-indigo-400 py-2 px-4 mb-1 rounded font-semibold">
                   Comprar <ShoppingCart />
+                </button>
+                <button className="md:w-52 text-center flex items-center justify-center md:justify-between gap-2 bg-indigo-200 py-2 px-4 rounded font-semibold">
+                  Adicionar ao carrinho
                 </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-
       <Footer />
     </>
   )
