e71a867f8072061b40dc47aa1eee04bd07c2eeed
Heitor Pereira <85217334+heitorkenzou@users.noreply.github.com>
2022-12-11 00:53:08 -0300

    Update index.jsx (header)
    add dropdown in categories

--- react-marketplace/src/components/Header/index.jsx
+++ react-marketplace/src/components/Header/index.jsx
@@ -4,15 +4,19 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/authContext'
 import GGH from '../../assets/GGHNEW.svg'
 import cart from '../../assets/cart.svg'
+import classNames from 'classnames'
 
 export default function Header() {
   const [mobileMenu, setMobileMenu] = useState(false)
   const itemNavBarClassName =
-    'flex transition ease-in-out my-2 p-3 hover:bg-indigo-200 rounded-md cursor-pointer text-black text-sm'
+    'flex transition ease-in-out my-2 p-3 flex items-center hover:bg-indigo-200 rounded-md cursor-pointer text-black text-sm'
 
   const { user, logout, loading } = useAuth()
   const navigate = useNavigate()
 
+  const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => setShowDropdown(!showDropdown);
   function changeProfile() {
     if (user) {
       return (
@@ -112,7 +116,31 @@ export default function Header() {
                   : 'gap-3 md:flex justify-between md:items-center justify-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4  md:p-0 p-6 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in-out duration-500 bg-white'
               }
             >
-              <p className={itemNavBarClassName}>Categorias</p>
+              <div>
+              <button className={itemNavBarClassName} onClick={toggleDropdown}>
+                Categorias
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-2">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+</svg>
+                </button>
+                <ul className={classNames('flex flex-col absolute bg-white rounded shadow-lg mt-2 py-2 z-50', { 'hidden': !showDropdown })}>
+        <li className="flex">
+          
+          <a href="#" className="block flex items-center px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={toggleDropdown}>D
+          item 1<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="flex-end ml-2 w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+</svg>
+</a>
+          
+        </li>
+        <li className="border-gray-400">
+          <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={toggleDropdown}>Dropdown item 2</a>
+        </li>
+        <li className="border-gray-400">
+          <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={toggleDropdown}>Dropdown item 3</a>
+        </li>
+      </ul>
+                </div>
               <Link className={itemNavBarClassName} to="/produtos">
                 Produtos recentes
               </Link>
